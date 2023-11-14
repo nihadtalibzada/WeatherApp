57c98bc568535af35973c325ce24f34b820a0752
Nihad Talibzada <nihad.talibzada@valtech.com>
2023-11-14 01:06:38 +0100

    Removed commented code

--- src/components/weather/SearchForecast.vue
+++ src/components/weather/SearchForecast.vue
@@ -13,19 +13,6 @@
       @keydown.enter="searchWeatherForecast"
       @click:append="searchWeatherForecast"
   ></v-text-field>
-  <!--            <v-combobox
-                      :items="cities"
-                      v-model="cityInput"
-                      @update:search-input="fetchCities"
-                      @change="searchWeatherForecast"
-                      color="white"
-                      outlined
-                      class="rounded-lg text-left"
-                      hide-no-data
-                      hide-selected
-                      label="Search"
-                      placeholder="Search a city"
-                  ></v-combobox>-->
 </template>
 
 <script>
@@ -37,9 +24,7 @@ export default {
   data() {
     return {
       cityInput: null,
-      requiredRule: [],
-      //cities: [],
-      //citySearch: null,
+      requiredRule: []
     }
   },
 
@@ -63,6 +48,7 @@ export default {
         return;
       }
       this.requiredRule = [];
+
       this.$store.commit('setLoading', true);
       this.fetchCurrentWeather(this.cityInput)
           .then()
@@ -73,30 +59,7 @@ export default {
       this.fetchWeatherForecast(this.cityInput)
           .then()
           .catch((error) => this.requiredRule = [error.message]);
-    },
-
-    /*fetchCities(input) {
-      console.log('EVEN: ', input)
-      const username = "ntalibzada";
-      const apiUrl = `http://api.geonames.org/searchJSON?name_equals=${input ?? input}&maxRows=10&username=${username}`;
-
-      if (input !== '' || input !== null) {
-        fetch(apiUrl)
-            .then(response => {
-              if (!response.ok) {
-                throw new Error("Network response was not ok");
-              }
-              return response.json();
-            })
-            .then(data => {
-              this.cities = data?.geonames?.map(city => `${city.name}, ${city.countryName}`);
-              console.log('cities: ', this.cities);
-            })
-            .catch(error => {
-              console.error("Error fetching cities:", error);
-            });
-      }
-    },*/
+    }
   }
 }
 </script>
